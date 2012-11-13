85326ad36b4e366a00bac73c5731f70e3915ead4
supernelis <nelis@archiwise.com>
2012-11-13 06:09:17 +0100

    First version of a node bot

---
+++ examples/bot_example.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: nelis
+ * Date: 12/11/12
+ * Time: 22:34
+ * To change this template use File | Settings | File Templates.
+ */
+
+const BotXmppHelper = require('../lib/bot_xmpp_helper');
+const xmpp = require('node-xmpp');
+
+const bot = new BotXmppHelper('joligeheidi@jabber.org','asjemenou', 'jabber.org');
