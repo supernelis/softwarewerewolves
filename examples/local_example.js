99229d2d62d08ce00b568e4a817dffce8d067b45
yo <yo@johanpeeters.com>
2012-12-01 14:05:36 +0100

    IP address for local server at XP Days

--- examples/local_example.js
+++ examples/local_example.js
@@ -1,9 +1,9 @@
 const GameEngine = require('../lib/game_engine');
 
-var gameCoordinator = 'sww@192.168.1.156';
+var gameCoordinator = 'sww@10.0.1.3';
 var gameCoordinatorPw = 's0ftwarew0lf';
-var xmppSrv = '192.168.1.156';
-var moderator = 'softwarewolf@192.168.1.156';
+var xmppSrv = '10.0.1.3';
+var moderator = 'softwarewolf@10.0.1.3';
 var moderatorPw = 's0ftwarew0lf';
 
 new GameEngine(gameCoordinator, gameCoordinatorPw, moderator, moderatorPw, xmppSrv);
